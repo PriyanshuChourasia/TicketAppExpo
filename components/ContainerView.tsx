76edd446bd94db1ca9fb7db675736f6f3037583b
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-01-02 15:38:49 +0530

    Native wind setup for tailwind css is done

---
+++ components/ContainerView.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+const ContainerView = ({children}:any) =>{
+    return(
+        <>
+        <View>
+            <Text>Hello World</Text>
+        </View>
+        </>
+    )
+}
+
+
+
+export default ContainerView;
